9e24e7b67ddf13734fb2b4090c4f24cea3e3f497
babluIsNotBodmash <106914485+babluIsNotBodmash@users.noreply.github.com>
2022-07-06 15:23:56 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (89.63%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    void calc_disp(int ind, vector<int> &nums,vector<int> &temp, vector<vector<int>> &ans){
+        ans.push_back(temp);
+        
+        for(int i=ind;i<nums.size();i++){
+            if(i!=ind && nums[i]==nums[i-1])continue;
+            
+            temp.push_back(nums[i]);
+            calc_disp(i+1,nums,temp,ans);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(nums.begin(),nums.end());
+        calc_disp(0,nums,temp,ans);
+        return ans;
+    }
+};
