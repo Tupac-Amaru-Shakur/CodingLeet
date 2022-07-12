fc2eb8d3d0eadc93b934dff186754b392f227b1d
babluIsNotBodmash <106914485+babluIsNotBodmash@users.noreply.github.com>
2022-07-12 12:45:48 +0530

    Time: 113 ms (80.11%), Space: 53.6 MB (19.93%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto &i:nums){
+            mp[i]++;
+            // if(mp[i]>1)
+            //     return false;
+        }
+        // return true;
+        for(auto &i:mp){
+            if(i.second>=2)
+                return true;
+        }
+        return false;
+    }
+};
