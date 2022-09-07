969a3ce2467b2c544fd5ae57847e0a2b53c92df2
babluIsNotBodmash <106914485+babluIsNotBodmash@users.noreply.github.com>
2022-09-07 11:09:16 +0530

    Time: 232 ms (5.44%), Space: 24.1 MB (67.81%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -14,54 +14,75 @@ class Solution
 public:
     vector<int> productExceptSelf(vector<int> &nums)
     {
-        int prod = 1;
-        vector<int> submit;
-        // int count = (nums.begin(), nums.end(), 0);
-        int count=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0)
-                count++;
+        // int prod = 1;
+        // vector<int> submit;
+        // // int count = (nums.begin(), nums.end(), 0);
+        // int count=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]==0)
+        //         count++;
+        // }
+        // if (count > 1)
+        // {
+        //     vector<int> final(nums.size());
+        //     return final;
+        // }
+        // else if (count == 1)
+        // {
+        //     for (int i = 0; i < nums.size(); i++)
+        //     {
+        //         if (nums[i] != 0)
+        //             prod *= nums[i];
+        //     }
+        //     for (int i = 0; i < nums.size(); i++)
+        //     {
+        //         if (nums[i] == 0)
+        //         {
+        //             submit.push_back(prod);
+        //         }
+        //         else
+        //         {
+        //             submit.push_back(0);
+        //         }
+        //     }
+        //     return submit;
+        // }
+        // else if (count == 0)
+        // {
+        //     for (int i = 0; i < nums.size(); i++)
+        //     {
+        //         // if (nums[i] == 0)
+        //         prod *= nums[i];
+        //     }
+        //     for (int i = 0; i < nums.size(); i++)
+        //     {
+        //         // if (nums[i] == 0)
+        //         int term = prod / nums[i];
+        //         submit.push_back(term);
+        //     }
+        // }
+        // return submit;
+        
+        int n=nums.size();
+        vector<int> ans(n,1);
+        int pro1=nums[0];
+        for(int i=1;i<n;i++){
+            
+            ans[i]=pro1;
+            pro1*=nums[i];
         }
-        if (count > 1)
-        {
-            vector<int> final(nums.size());
-            return final;
+        
+        for(auto &i:ans){
+            cout<<i<<endl;
         }
-        else if (count == 1)
-        {
-            for (int i = 0; i < nums.size(); i++)
-            {
-                if (nums[i] != 0)
-                    prod *= nums[i];
-            }
-            for (int i = 0; i < nums.size(); i++)
-            {
-                if (nums[i] == 0)
-                {
-                    submit.push_back(prod);
-                }
-                else
-                {
-                    submit.push_back(0);
-                }
-            }
-            return submit;
+        
+        int post=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            ans[i]*=post;
+            post*=nums[i];
         }
-        else if (count == 0)
-        {
-            for (int i = 0; i < nums.size(); i++)
-            {
-                // if (nums[i] == 0)
-                prod *= nums[i];
-            }
-            for (int i = 0; i < nums.size(); i++)
-            {
-                // if (nums[i] == 0)
-                int term = prod / nums[i];
-                submit.push_back(term);
-            }
-        }
-        return submit;
+        
+        return ans;
     }
 };
