ec2a223fa5b59174d5dbff2db5ab6e694dd0a0b8
babluIsNotBodmash <106914485+babluIsNotBodmash@users.noreply.github.com>
2022-09-08 19:54:29 +0530

    Time: 60 ms (22.34%), Space: 10.9 MB (25.63%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,46 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int arr[256]={0};
-        int i=0;
-        int j=0;
-        int len=size(s);
-        int max=0;
-        while(i<len && j<len)
+class Solution
+{
+    public:
+        int lengthOfLongestSubstring(string s)
         {
-            if(arr[s[j]]==0)
-            {
-                arr[s[j]]=1;j++;
-                max=max>j-i?max:j-i;
-            }else{
-                arr[s[i]]=0;
-                i++;
+            // int arr[256] = { 0 };
+            // int i = 0;
+            // int j = 0;
+            // int len = size(s);
+            // int max = 0;
+            // while (i < len && j < len)
+            // {
+            //     if (arr[s[j]] == 0)
+            //     {
+            //         arr[s[j]] = 1;
+            //         j++;
+            //         max = max > j - i ? max : j - i;
+            //     }
+            //     else
+            //     {
+            //         arr[s[i]] = 0;
+            //         i++;
+            //     }
+            // }
+            // return max;
+            
+            int n=s.size();
+            int i=0;
+            int j=0;
+            int ans=0;
+            unordered_set<char>ss;
+            while(j<n){
+                if(ss.find(s[j])==ss.end()){
+                    ss.insert(s[j]);
+                    ans=max(ans,j-i+1);
+                    j++;
+                }
+                else{
+                    ss.erase(s[i]);
+                    i++;
+                }
             }
+            
+            return ans;
         }
-    return max;}
 };
