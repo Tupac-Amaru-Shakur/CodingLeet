428fa26a86356f86c1f2ee2fe0bc569a681f4eb2
babluIsNotBodmash <106914485+babluIsNotBodmash@users.noreply.github.com>
2022-09-12 02:18:20 +0530

    Time: 11 ms (55.64%), Space: 13.6 MB (96.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(1005);
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        
         for(int i=2;i<cost.size();i++){
-            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+            cost[i]+=min(cost[i-1],cost[i-2]);
         }
         
-        return min(dp[cost.size()-1],dp[cost.size()-2]);
+        return min(cost[cost.size()-1],cost[cost.size()-2]);
     }
 };
